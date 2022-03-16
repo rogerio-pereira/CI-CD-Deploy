ec086e434d8b9872bfc834a3d0d4ddd8ee34b964
Rogerio Pereira <rpereira@acslabtest.com>
2022-03-16 14:54:30 -0400

    Change menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">New</a>
                         @endif
                     @endauth
                 </div>
